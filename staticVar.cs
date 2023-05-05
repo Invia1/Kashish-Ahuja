e3a64253caf3d2cb757f638c30281d7e560b7781
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-05 15:13:54 +0530

    Add files via upload

---
+++ staticVar.cs
@@ -0,0 +1,16 @@
+
+class college
+{
+    public static string adress;
+    public static string name;
+
+    public static void display(){
+        name = "Kashish";
+        adress = " Noida";
+        Console.WriteLine(adress+" " + name);
+
+    }
+    public static void Main(){
+        display();
+    }
+}
