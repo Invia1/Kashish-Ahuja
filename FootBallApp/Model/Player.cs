155b163315973218bca1417d7c2eb12a1f6ec86c
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-18 09:18:38 +0530

    Add files via upload

---
+++ FootBallApp/Model/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FinalAssignment.Model
+{
+    internal class Player
+    {
+        public int JerseyNo { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+
+        public string country { get; set; }
+    }
+}
---
+++ FootBallApp/ServiceFunction/Interface1.cs
@@ -0,0 +1,15 @@
+﻿using FinalAssignment.Model;
+using System;
+
+
+namespace FinalAssignment.ServiceFunction
+{
+    internal class Interface1
+    {
+       public  int CreateStudent(Player player);
+        List<Player> GetList();
+       public  void Update(int id);
+        public void Delete(int id);
+        public void search(int id);
+    }
+}
