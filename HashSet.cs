145b177db30f9770fb6c1e235a61357585fde8e6
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-19 10:33:14 +0530

    Add files via upload

---
+++ HashSet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class HashSet
+{
+        public static void Main()
+    {
+        HashSet<int> set1 = new HashSet<int>();
+
+        // Add Elements in HashSet
+
+        for (int i = 1; i <= 10; i++)
+        {
+            set1.Add(i);
+        }
+
+        // Display the elements in HashSet
+        Console.WriteLine("The elements in the HashSet are:");
+        foreach (int i in set1)
+        {
+            Console.WriteLine(i);
+        }
+
+        // Removing all even elements from set
+
+        Console.WriteLine(" Remove all even elements and display all odd numbers : ");
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                set1.Remove(i);
+            }
+        }
+        Console.WriteLine("The odd elements in the HashSet are:");
+        foreach (int i in set1)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
