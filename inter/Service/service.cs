fa3177e5413d53b04bac793fd3374c8861e0fc14
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-22 16:25:42 +0530

    Add files via upload

---
+++ inter/Service/service.cs
@@ -0,0 +1,27 @@
+﻿using inter.Interface;
+using inter.Model;
+using System;
+
+
+namespace inter.Service
+{
+    public class service : Interface1
+    {
+        employee emp1 = new employee();
+
+        public void setDetails()
+        {
+            Console.WriteLine("Enter Employee Id : ");
+            emp1.EmpId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Name : ");
+            emp1.Name = Console.ReadLine();
+        }
+       
+        public employee getDetails()
+        {
+            return emp1;
+        }
+
+    }
+}
---
+++ inter/UI/main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using inter.Service;
+using inter.Interface;
+using inter.Model;
+
+namespace inter.UI
+{
+    public class main
+    {
+
+        public  static void Main()
+        {
+            employee obj = new employee();
+            Interface1 obj1 = new service();
+
+            obj1.setDetails();
+             employee temp = obj1.getDetails();
+            Console.WriteLine("Employee Id : " + temp.EmpId + "Employee Name : "+temp.Name);
+        }
+    }
+}
